c8230abd52dcad14ad06f161853cbd74f52b665c
Nam <efert269@gmail.com>
2020-11-01 14:54:33 +0700

    setting

--- myblog/settings.py
+++ myblog/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog',
     'home',
-    
+    'whitenoise.runserver_nostatic',
+
 ]
 
 MIDDLEWARE = [
--- myblog/urls.py
+++ myblog/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
